30f564cbb09825e14e95d327f9c903d6918f3055
jonel-zape <jonel.zape@outlook.com>
2019-12-15 21:48:54 +0800

    :star: Updated JQuery library

--- app/resources/pages/master/footer.php
+++ app/resources/pages/master/footer.php
@@ -1,2 +1,5 @@
+        <script src="js/lib/jquery-3.4.1.min.js"></script>
+        <script src="js/template/bootstrap.min.js"></script>
+        <script src="js/template/Chart.min.js"></script>
     </body>
 </html>
--- app/resources/pages/master/header.php
+++ app/resources/pages/master/header.php
@@ -2,15 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
         <title>Sales Management</title>
         <meta name="keywords" content="" />
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="css/templatemo_main.css">
-        <!--
-        Dashboard Template
-        http://www.templatemo.com/preview/templatemo_415_dashboard
-        -->
     </head>
     <body>
