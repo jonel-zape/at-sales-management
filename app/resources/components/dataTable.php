97b57439377b9e3e5a37413a6f9cfc5c47a46a7f
jonel-zape <jonel.zape@outlook.com>
2019-12-22 15:30:45 +0800

    :star: Added Tabulator component

---
+++ app/resources/components/dataTable.php
@@ -0,0 +1,13 @@
+<div id="data-table"></div>
+
+<script type="text/javascript">
+
+    let dataTable = new Tabulator("#data-table", {
+        layout: "fitDataStretch",
+        autoColumns: false,
+        pagination: "local",
+        paginationSize: 10,
+        paginationSizeSelector: [5, 10, 50, 100, 200],
+    });
+
+</script>
--- public/js/modules/product/list.js
+++ public/js/modules/product/list.js
@@ -1,5 +1,35 @@
 let list = {
     find() {
         loading.show();
+
+        let that = this;
+        http.get(
+            '/product/find',
+            {
+                keyword: el.val("#keyword"),
+                filterBy: el.val("#filterBy")
+            }
+        ).done(function(response){
+            that.setDataTableColumns();
+            dataTable.setData(response.values);
+            loading.hide();
+        }).catch(function(response){
+            loading.hide();
+        });
+    },
+
+    setDataTableColumns() {
+        let columns = [
+            { title:"Stock No.", field:"stock_no", formatter:"plaintext" },
+            { title:"Name", field:"name", formatter:"plaintext"},
+            { title:"Short Name", field:"short_name", formatter:"plaintext" },
+            { title:"Cost Price", field:"cost_price", formatter:"money" },
+            { title:"Selling Price", field:"selling_price", formatter:"money"},
+            { title:"Wholesale Price", field:"wholesale_price", formatter:"money"},
+            { title:"Memo", field:"memo" },
+        ];
+        dataTable.setColumns(columns);
     }
 };
+
+list.find();
