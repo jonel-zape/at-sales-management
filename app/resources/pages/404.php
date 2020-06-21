3b1ef7eba678f5cbcc947eed13ed1020d27753b3
Jonel Zape <jonel.zape@serino.com>
2020-06-21 18:31:29 +0800

    :star: added gif in 404 page

--- app/resources/pages/404.php
+++ app/resources/pages/404.php
@@ -12,6 +12,7 @@
         <div class="alert alert-danger alert-dismissible" role="alert">
             404 Page not found.
         </div>
+        <img src="/images/no.gif" height="300" />
     </div>
 </div>
 
